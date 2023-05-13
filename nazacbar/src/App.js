b390c99707101268af5e2689c5c731e2f2f324cc
wootaegyeoung <118430480+wootaegyeoung@users.noreply.github.com>
2023-05-14 08:11:44 +0900

    Update App.js

--- nazacbar/src/App.js
+++ nazacbar/src/App.js
@@ -3,16 +3,16 @@ import News from "./pages/News";
 import Board from "./pages/Board";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import Login from "./pages/Login"
 import Layout from './pages/Layout'
 import NoPage from "./pages/NoPage";
 import Topbar from "./components/TopBar";
 import Footer from "./components/Footer";
-import BoardList from "./pages/BoardList";
-import BoardDetail from "./pages/BoardDetail";
+
 
 import { useState, useEffect } from "react";
 
-
+//1235
 function App() {
 
 
@@ -28,8 +28,7 @@ function App() {
           <Route path="news" element={<News />} />
           <Route path="products" element={<Products />} />
           <Route path="board" element={<Board />} />
-          <Route path="boardlist" element={<BoardList />}/>
-          <Route exact path="/board/detail/:id" element={<BoardDetail />} />
+          <Route path="login" element={<Login />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
