1ef517ba4d315bf7995c0f60ffcce93103784f47
wootaegyeoung <118430480+wootaegyeoung@users.noreply.github.com>
2023-05-14 14:04:39 +0900

    Woo
    로그인 상태유지도 끝

--- nazacbar/src/pages/Login.js
+++ nazacbar/src/pages/Login.js
@@ -7,8 +7,7 @@ const Login = () =>{
     const baseUrl = "http://localhost:8084";
     const [inputMail, setInputMail] = useState('')
     const [inputPw, setInputPw] = useState('')
-    const [user, setUser] = useState([]);
-
+    const [loggedInUser, setLoggedInUser] = useState(null);
     const [showSignUp, setShowSignUp] = useState(false);
 
     const handleInputMail = (e) => {
@@ -19,35 +18,54 @@ const Login = () =>{
         setInputPw(e.target.value)
     }
 
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('loggedInUser'));
+        if (user) {
+            setLoggedInUser(user);
+        }
+    }, []);
+
     async function onClickLogin(){
-        axios.get(`${baseUrl}/api/user/${inputMail}`).then((response) => setUser(response.data))
-        
-        if(user.password!==inputPw){
+        const response = await axios.get(`${baseUrl}/api/user/${inputMail}`);
+        const user = response.data;
+            
+        if(user.password !== inputPw){
             console.log("NO")
             alert("find not user information")
-        }else {
-            Topbar.log_name="user : "+user.name
-            console.log(Topbar.log_name)
+        } else {
+            localStorage.setItem('loggedInUser', JSON.stringify(user));
+            setLoggedInUser(user);
+            console.log("user logged in: ", user);
             alert("login")
-            
         }
     }
 
     function onClickSignUp() {
         setShowSignUp(true);
-      }
+    }
       
-      function onCloseSignUp() {
+    function onCloseSignUp() {
         setShowSignUp(false);
-      }
+    }
 
+    // 로그인이 되어있을 때에만 사용자 정보화면을 렌더링합니다.
+    if (loggedInUser) {
+        return (
+            <div>
+                <Topbar log_name={`user: ${loggedInUser.name}`} />
+                <h1>Welcome {loggedInUser.name}!</h1>
+                <button onClick={() => setLoggedInUser(null)}>Logout</button>
+            </div>
+        );
+    }
+
+    // 로그인이 되어있지 않은 경우에는 로그인 화면을 렌더링합니다.
     return(
         <div style={{ 
             display: 'flex', justifyContent: 'center', alignItems: 'center', 
             width: '100%', height: '100vh'
-            }}>
-            <form style={{ display: 'flex', flexDirection: 'column'}}
-            >
+        }}>
+            <form style={{ display: 'flex', flexDirection: 'column'}}>
                 <label>Email</label>
                 <input type='email' value={inputMail} onChange={handleInputMail} placeholder="ID"/>
                 <label>Password</label>
