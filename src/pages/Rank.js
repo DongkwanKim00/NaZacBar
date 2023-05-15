aebcc7e2f2721d5eaac9aa8a57a8a210e0fc3879
DongkwanKim00 <dkkim_it@naver.com>
2023-05-16 01:14:25 +0900

    랭킹 카테고리별 업데이트

--- src/pages/Rank.js
+++ src/pages/Rank.js
@@ -19,40 +19,121 @@ const Rank = () => {
     }
   };
 
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
   return (
+
     <div style={{ background: `url('/barImage2.png') no-repeat center center fixed`, WebkitBackgroundSize: 'cover', MozBackgroundSize: 'cover', OBackgroundSize: 'cover', backgroundSize: 'cover', minHeight: '100vh', padding: '50px' }}>
-      <h1 style={{ textAlign: "center", color: "#ffffff", position: "fixed", left: "38%"}}>카테고리별 인기 게시글</h1>
-      {/* <div style={{ display: "flex", justifyContent: "center", position: "sticky"}}>  */}
-      <div style={{ left: "20%", width: "60%", marginTop: "100px", backgroundColor: "rgba(0, 0, 0, 0.5)", position: "fixed", maxHeight: "calc(100vh - 300px)", overflowY: "auto" }}>
-      <table style={{ width: "100%" }}>
-          <thead>
-            <tr>
-              <th style={{ color: "#ffffff", borderBottom: "1px solid #ffffff", padding: "10px" }}>카테고리</th>
-              <th style={{ color: "#ffffff", borderBottom: "1px solid #ffffff", padding: "10px" }}>제목</th>
-              <th style={{ color: "#ffffff", borderBottom: "1px solid #ffffff", padding: "10px" }}>추천 수</th>
-              <th style={{ color: "#ffffff", borderBottom: "1px solid #ffffff", padding: "10px" }}>작성자</th>
-              <th style={{ color: "#ffffff", borderBottom: "1px solid #ffffff", padding: "10px" }}>작성일</th>
-            </tr>
-          </thead>
-          <tbody>
-            {rankedList.map((post) => (
-              <tr key={post.id}>
-                <td style={{ color: "#ffffff", padding: "10px" }}>{post.category}</td>
-                <td style={{ color: "#ffffff", padding: "10px" }}>
-                  {post.image && (
-                    <img src={`${baseUrl}/api/board/image/${post.id}`} alt="게시글 이미지" style={{ width: "50px", height: "auto", marginRight: "10px" }} />
-                  )}
-                  <Link to={`/board/detail/${post.id}`} style={{ color: "#ffffff", textDecoration: "underline" }}>
-                    {post.title}
-                  </Link>
-                </td>
-                <td style={{ color: "#ffffff", padding: "10px" }}>{post.recommendation}</td>
-                <td style={{ color: "#ffffff", padding: "10px" }}>{post.author}</td>
-                <td style={{ color: "#ffffff", padding: "10px" }}>{new Date(post.createdAt).toLocaleString()}</td>
+      <h1 style={{ textAlign: "center", color: "#ffffff", position: "fixed", left: "38%" }}>카테고리별 인기 게시글</h1>
+
+      <div>
+        <input type="checkbox" name="rank" value="wisky" checked={selectedOption === "wisky"} onChange={handleOptionChange} style={{ marginLeft: "15px"}}/> <label style={{ color: "#ffffff" , marginRight: "10px"}}>Wisky</label>
+        <input type="checkbox" name="rank" value="beer" checked={selectedOption === "beer"} onChange={handleOptionChange} /> <label style={{ color: "#ffffff" , marginRight: "10px"}}>Beer</label>
+        <input type="checkbox" name="rank" value="soju" checked={selectedOption === "soju"} onChange={handleOptionChange} /> <label style={{ color: "#ffffff" , marginRight: "10px"}}>Soju</label>
+      </div>
+
+      <div id="onoffDisplay">
+        {selectedOption === 'wisky' && <div id="wiskyDisplay" style={{ left: "20%", width: "60%", marginTop: "100px", backgroundColor: "rgba(0, 0, 0, 0.5)", position: "fixed", maxHeight: "calc(100vh - 300px)", overflowY: "auto" }}>
+          <table style={{ width: "100%" }}>
+            <thead>
+              <tr>
+                <th style={{ color: "#ffffff", borderBottom: "1px solid #ffffff", padding: "10px" }}>카테고리111</th>
+                <th style={{ color: "#ffffff", borderBottom: "1px solid #ffffff", padding: "10px" }}>제목</th>
+                <th style={{ color: "#ffffff", borderBottom: "1px solid #ffffff", padding: "10px" }}>추천 수</th>
+                <th style={{ color: "#ffffff", borderBottom: "1px solid #ffffff", padding: "10px" }}>작성자</th>
+                <th style={{ color: "#ffffff", borderBottom: "1px solid #ffffff", padding: "10px" }}>작성일</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {rankedList.filter((post) => post.category === "Whiskey").map((post) => (
+                <tr key={post.id}>
+                  <td style={{ color: "#ffffff", padding: "10px" }}>{post.category}</td>
+                  <td style={{ color: "#ffffff", padding: "10px" }}>
+                    {post.image && (
+                      <img src={`${baseUrl}/api/board/image/${post.id}`} alt="게시글 이미지" style={{ width: "50px", height: "auto", marginRight: "10px" }} />
+                    )}
+                    <Link to={`/board/detail/${post.id}`} style={{ color: "#ffffff", textDecoration: "underline" }}>
+                      {post.title}
+                    </Link>
+                  </td>
+                  <td style={{ color: "#ffffff", padding: "10px" }}>{post.recommendation}</td>
+                  <td style={{ color: "#ffffff", padding: "10px" }}>{post.author}</td>
+                  <td style={{ color: "#ffffff", padding: "10px" }}>{new Date(post.createdAt).toLocaleString()}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        }
+
+        {selectedOption === 'beer' && <div id="beerDisplay" style={{ left: "20%", width: "60%", marginTop: "100px", backgroundColor: "rgba(0, 0, 0, 0.5)", position: "fixed", maxHeight: "calc(100vh - 300px)", overflowY: "auto" }}>
+          <table style={{ width: "100%" }}>
+            <thead>
+              <tr>
+                <th style={{ color: "#ffffff", borderBottom: "1px solid #ffffff", padding: "10px" }}>카테고리222</th>
+                <th style={{ color: "#ffffff", borderBottom: "1px solid #ffffff", padding: "10px" }}>제목</th>
+                <th style={{ color: "#ffffff", borderBottom: "1px solid #ffffff", padding: "10px" }}>추천 수</th>
+                <th style={{ color: "#ffffff", borderBottom: "1px solid #ffffff", padding: "10px" }}>작성자</th>
+                <th style={{ color: "#ffffff", borderBottom: "1px solid #ffffff", padding: "10px" }}>작성일</th>
+              </tr>
+            </thead>
+            <tbody>
+              {rankedList.filter((post) => post.category === "Beer").map((post) => (
+                <tr key={post.id}>
+                  <td style={{ color: "#ffffff", padding: "10px" }}>{post.category}</td>
+                  <td style={{ color: "#ffffff", padding: "10px" }}>
+                    {post.image && (
+                      <img src={`${baseUrl}/api/board/image/${post.id}`} alt="게시글 이미지" style={{ width: "50px", height: "auto", marginRight: "10px" }} />
+                    )}
+                    <Link to={`/board/detail/${post.id}`} style={{ color: "#ffffff", textDecoration: "underline" }}>
+                      {post.title}
+                    </Link>
+                  </td>
+                  <td style={{ color: "#ffffff", padding: "10px" }}>{post.recommendation}</td>
+                  <td style={{ color: "#ffffff", padding: "10px" }}>{post.author}</td>
+                  <td style={{ color: "#ffffff", padding: "10px" }}>{new Date(post.createdAt).toLocaleString()}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        }
+        {selectedOption === 'soju' && <div id="sojuDisplay" style={{ left: "20%", width: "60%", marginTop: "100px", backgroundColor: "rgba(0, 0, 0, 0.5)", position: "fixed", maxHeight: "calc(100vh - 300px)", overflowY: "auto" }}>
+          <table style={{ width: "100%" }}>
+            <thead>
+              <tr>
+                <th style={{ color: "#ffffff", borderBottom: "1px solid #ffffff", padding: "10px" }}>카테고리333</th>
+                <th style={{ color: "#ffffff", borderBottom: "1px solid #ffffff", padding: "10px" }}>제목</th>
+                <th style={{ color: "#ffffff", borderBottom: "1px solid #ffffff", padding: "10px" }}>추천 수</th>
+                <th style={{ color: "#ffffff", borderBottom: "1px solid #ffffff", padding: "10px" }}>작성자</th>
+                <th style={{ color: "#ffffff", borderBottom: "1px solid #ffffff", padding: "10px" }}>작성일</th>
+              </tr>
+            </thead>
+            <tbody>
+              {rankedList.filter((post) => post.category === "Soju").map((post) => (
+                <tr key={post.id}>
+                  <td style={{ color: "#ffffff", padding: "10px" }}>{post.category}</td>
+                  <td style={{ color: "#ffffff", padding: "10px" }}>
+                    {post.image && (
+                      <img src={`${baseUrl}/api/board/image/${post.id}`} alt="게시글 이미지" style={{ width: "50px", height: "auto", marginRight: "10px" }} />
+                    )}
+                    <Link to={`/board/detail/${post.id}`} style={{ color: "#ffffff", textDecoration: "underline" }}>
+                      {post.title}
+                    </Link>
+                  </td>
+                  <td style={{ color: "#ffffff", padding: "10px" }}>{post.recommendation}</td>
+                  <td style={{ color: "#ffffff", padding: "10px" }}>{post.author}</td>
+                  <td style={{ color: "#ffffff", padding: "10px" }}>{new Date(post.createdAt).toLocaleString()}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        }
       </div>
     </div>
   );
