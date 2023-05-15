e40e2aafe62e7d84ef34d9eb3cc6de391f014905
DongkwanKim00 <dkkim_it@naver.com>
2023-05-15 23:27:46 +0900

    메인로고 업뎃

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -8,7 +8,7 @@ var log_href="/login"
 const Topbar = () => {
     return (
         <Navbar bg = "dark" variant = "dark" expand = "lg" className = "d-flex justify-content-center w-100" style={{ position: 'fixed', top: 0, zIndex: 1 }}>
-            <Navbar.Brand href="/" className="text-center w-100">나작바</Navbar.Brand>
+            <Navbar.Brand href="/" className="text-center w-100"><img src="/logo.png"/></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav" className = "text-center w-100">
             <Nav className="mx-auto">
