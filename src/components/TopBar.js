08de47f080dd3b436e2419b66a862a59c8369706
wootaegyeoung <118430480+wootaegyeoung@users.noreply.github.com>
2023-05-16 21:16:36 +0900

    Woo
    로그인 수정 및 탑바 수정

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,28 +1,51 @@
-import React from "react";
-import { Navbar, Nav, Form, FormControl } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Navbar, Nav } from "react-bootstrap";
 import "../index.css";
 
-var log_name="Login"
-var log_href="/login"
-
 const Topbar = () => {
-    return (
-        <Navbar bg = "dark" variant = "dark" expand = "lg" className = "d-flex justify-content-center w-100" style={{ position: 'fixed', top: 0, zIndex: 1 }}>
-            <Navbar.Brand href="/" className="text-center w-100"><img src="/logo.png"/></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav" className = "text-center w-100">
-            <Nav className="mx-auto">
-              <Nav.Link href="/rank">Rank</Nav.Link>
-              <Nav.Link href="/products">Products</Nav.Link>
+  const [loginText, setLoginText] = useState("Login");
 
-              <Nav.Link href="/boardlist">Board</Nav.Link>
-              
-              <Nav.Link href={log_href}>{log_name}</Nav.Link>
+  useEffect(() => {
+    const loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
 
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-    );
-}
+    const updateLoginText = () => {
+      const author = loggedInUser ? loggedInUser.name : "";
+      setLoginText(loggedInUser ? `User: ${author}` : "Login");
+    };
+
+    updateLoginText(); // Update immediately on component mount
+
+    // Subscribe to changes in localStorage
+    window.addEventListener("storage", updateLoginText);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("storage", updateLoginText);
+    };
+  }, []);
+
+  return (
+    <Navbar
+      bg="dark"
+      variant="dark"
+      expand="lg"
+      className="d-flex justify-content-center w-100"
+      style={{ position: "fixed", top: 0, zIndex: 1 }}
+    >
+      <Navbar.Brand href="/" className="text-center w-100">
+        <img src="/logo.png" alt="Logo" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav" className="text-center w-100">
+        <Nav className="mx-auto">
+          <Nav.Link href="/rank">Rank</Nav.Link>
+          <Nav.Link href="/products">Products</Nav.Link>
+          <Nav.Link href="/boardlist">Board</Nav.Link>
+          <Nav.Link href="/login">{loginText}</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
 export default Topbar;
