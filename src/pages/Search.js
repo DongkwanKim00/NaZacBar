991f9facce31778733c2b0a29357d4e02d4a30f9
MoonJungGon <mtbird77@naver.com>
2023-05-26 19:18:25 +0900

    홈 이미지 수정

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -56,17 +56,19 @@ class Search extends Component {
       
     };
     const elementStyle = {
-      border: 'solid',
-      borderRadius: '10px',
+      border: 'none',
+      padding: '20px',
       position: 'relative',
       left: '10vh',
       height: '8vh',
-      width: '40vh',
+      width: '42vh',
       marginTop: '5vh',
       marginBottom: '10vh',
-      borderColor: 'skyblue',
-      fontSize: '35px',
-      
+      backgroundColor: 'rgba(0, 0, 0, 0)',
+      fontSize: '38px',
+      backgroundImage: `url(${process.env.PUBLIC_URL}/searchButton.png)`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
     };
 
     const items = Information.filter((data) => {
@@ -114,7 +116,16 @@ class Search extends Component {
             placeholder="Enter "
             style={elementStyle}
             onKeyDown={(e) => this.searchSpace(e)}
+            
           />
+
+          <img src="/sojuTestTest.png"  style={{
+      width: '100%', // 이미지의 너비를 조정합니다. 원하는 크기로 변경하세요.
+      height: 'auto', // 이미지의 높이를 자동으로 조정합니다.
+      display: 'block', // 이미지를 블록 요소로 설정하여 가로 중앙 정렬을 적용합니다.
+      margin: '0 auto', // 가로 중앙 정렬을 위해 좌우 여백을 자동으로 조정합니다.
+      marginBottom: '-90px',
+    }} />
         </div>
         <div className="home-contents-area" style={{ backgroundImage: "url('/noteBackground.jpg')", backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center" }}>
   {checkClicked === 1 && selectedItem ? (
