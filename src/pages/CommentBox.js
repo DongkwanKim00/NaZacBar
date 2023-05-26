539ebc6e0b8e1de12120edd25c16f1c95b228b64
MoonJungGon <mtbird77@naver.com>
2023-05-27 00:27:17 +0900

    CommitBox 코드 8092 포트와 연동할 수 있게 수정

--- src/pages/CommentBox.js
+++ src/pages/CommentBox.js
@@ -1,90 +1,109 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { getComments, addComment } from './CommentList';
 import './Soju.css';
+import ChatData from './ChatData';
+import LoggedInUserContext from './LoggedInUserContext';
+import axios from 'axios';
 
 
-class CommentBox extends Component {
-  constructor() {
-    super();
-    this.state = {
-      comments: [],
-      newComment: '',
-      userName: 'John Doe', // 사용자 이름 예시
-    };
-  }
+const CommentBox = () => {
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState('');
+  const [userName, setUserName] = useState('John Doe'); // 사용자 이름 예시
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
+  
+  const loggedInUser = useContext(LoggedInUserContext);
+  const baseUrl = "http://localhost:8092";
 
-  componentDidMount() {
-    this.fetchComments();
-  }
+  useEffect(() => {
+    fetchComments();
+  }, []);
 
-  fetchComments = () => {
+  const fetchComments = () => {
     const comments = getComments();
-    this.setState({ comments });
+    setComments(comments);
   };
 
-  handleInputChange = (event) => {
-    this.setState({
-      newComment: event.target.value,
-    });
+  const handleInputChange = (event) => {
+    setNewComment(event.target.value);
   };
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const { newComment, comments } = this.state;
     if (newComment.trim() !== '') {
-      const updatedComments = [{ comment: newComment, user: this.state.userName }, ...comments];
-      this.setState({
-        comments: updatedComments,
-        newComment: '',
-      });
+      const updatedComments = [{ comment: newComment, user: userName }, ...comments];
+      setComments(updatedComments);
+      setNewComment('');
     }
   };
 
-  render() {
-    const { comments, newComment } = this.state;
+  const handleCommitSubmit = (e) => {
+    setAuthor(loggedInUser.mail);
+    setCategory(ChatData.category);
+    
+    if (newComment.trim() !== '') {
+      setComments(newComment);
+    }
 
-    return (
-      <div>
-        <div className="comment-box-container">
-          <form onSubmit={this.handleSubmit} className="comment-button-submit-location">
-            <div className="comment-input-container">
-              <textarea
-                className="comment-input"
-                value={newComment}
-                onChange={this.handleInputChange}
-                placeholder="Write a comment..."
-              ></textarea>
-            </div>
-            <button className="comment-submit-btn" type="submit">
-              Submit
-            </button>
-          </form>
-        </div>
-        <div className="comment-list">
-          {comments.length > 0 ? (
-            <ul className="comment-ul">
-              {comments.map((comment, index) => (
-                <li key={index} className="comment-item">
-                  <div className="comment-avatar">
-                    <img src="/basic_profile.png" alt="Profile" className="profile-image" />
-                    <span className="comment-username">칵테일 러버</span>
-                    <img src="/memoPin.jpg" alt="Memo Pin" className="memo-pin-image" />
-                  </div>
-                  <div className="comment-content">
-                    
-                    <span className="comment-text">{comment.comment}</span>
-                  </div>
-                  
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p className="no-comments">No comments yet</p>
-          )}
-        </div>
+    e.preventDefault();
+    console.log(category, author, comments);
+    axios
+      .post(`${baseUrl}/api/chat`, {
+        category: category,
+        author:  author,
+        content: comments,
+      })
+      .then((response) => {
+        console.log(response.data);
+        alert('댓글 입력 완료!');
+        
+      })
+      .catch((error) => {
+        console.log(error);
+        alert('댓글 입력 실패!');
+      });
+  };
+
+  return (
+    <div>
+      <div className="comment-box-container">
+        <form onSubmit={handleCommitSubmit} className="comment-button-submit-location">
+          <div className="comment-input-container">
+            <textarea
+              className="comment-input"
+              value={newComment}
+              onChange={handleInputChange}
+              placeholder="Write a comment..."
+            ></textarea>
+          </div>
+          <button className="comment-submit-btn" type="submit">
+            Submit
+          </button>
+        </form>
       </div>
-    );
-  }
-}
+      <div className="comment-list">
+        {comments.length > 0 ? (
+          <ul className="comment-ul">
+            {comments.map((comment, index) => (
+              <li key={index} className="comment-item">
+                <div className="comment-avatar">
+                  <img src="/basic_profile.png" alt="Profile" className="profile-image" />
+                  <span className="comment-username">칵테일 러버</span>
+                  <img src="/memoPin.jpg" alt="Memo Pin" className="memo-pin-image" />
+                </div>
+                <div className="comment-content">
+                  <span className="comment-text">{comment.comment}</span>
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className="no-comments">No comments yet</p>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default CommentBox;
